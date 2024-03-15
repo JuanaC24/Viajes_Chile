dab6dc98127dd7f14b9f7c67ac07136f3dbfde0a
Juana Cortez <j.cortezramos22@gmail.com>
2024-03-14 23:31:04 -0300

    primer commit prueba

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: "#navbar-example",
+  });
+  
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);});
+
+  
